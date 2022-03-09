8d3cde2f63b13f72ce347bdf3bdfa3830ccf83ef
Abderrahim Boudounit <aboudoun@e2r8p12.1337.ma>
2022-03-09 18:51:31 +0100

    moves are done

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:59:22 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/03/09 18:19:19 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/03/09 18:51:21 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ void	pa(t_stack *stack_a, t_stack *stack_b)
 
 	top_a = stack_a->top;
 	top_b = stack_b->top;
-	stack_a->array[top_a + 1] = stack_b->array[top_b];
-	stack_a->top += 1;
-	stack_b->top -= 1;
-	write(1, "pa\n", 3);
+	if (top_b >= 0)
+	{
+		stack_a->array[top_a + 1] = stack_b->array[top_b];
+		stack_a->top += 1;
+		stack_b->top -= 1;
+		write(1, "pa\n", 3);
+	}
 }
 
 void	pb(t_stack *stack_a, t_stack *stack_b)
@@ -32,8 +35,11 @@ void	pb(t_stack *stack_a, t_stack *stack_b)
 
 	top_a = stack_a->top;
 	top_b = stack_b->top;
-	stack_b->array[top_b + 1] = stack_a->array[top_a];
-	stack_b->top += 1;
-	stack_a->top -= 1;
-	write(1, "pb\n", 3);
+	if (top_a >= 0)
+	{
+		stack_b->array[top_b + 1] = stack_a->array[top_a];
+		stack_b->top += 1;
+		stack_a->top -= 1;
+		write(1, "pb\n", 3);
+	}
 }
