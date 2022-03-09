4ad40cb2b7e21bc8a099995f219d4e563d28224d
Abderrahim Boudounit <aboudoun@e2r8p12.1337.ma>
2022-03-09 17:06:26 +0100

    done

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 18:55:40 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/03/08 20:42:18 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/03/09 17:01:49 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,16 @@ typedef struct s_stack{
 void	exit_eroor(t_stack *a, t_stack *b);
 int		check_digit(int ac, char **av);
 int		stack(int ac, char **av, t_stack *stack_a, t_stack *stack_b);
+void	ra(t_stack *stack_a);
+void	rb(t_stack *stack_b);
+void	rr(t_stack *stack_a, t_stack *stack_b);
+void	rra(t_stack *stack_a);
+void	rrb(t_stack *stack_b);
+void	rrr(t_stack *stack_a, t_stack *stack_b);
+void	pa(t_stack *stack_a, t_stack *stack_b);
+void	pb(t_stack *stack_a, t_stack *stack_b);
+void	sa(t_stack *stack_a, t_stack *stack_b);
+void	sb(t_stack *stack_a, t_stack *stack_b);
+void	ss(t_stack *stack_a, t_stack *stack_b);
 
 #endif
